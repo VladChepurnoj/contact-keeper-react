16a924c2559d1a7175b3d9108df5375d2c38fdcb
unknown <chepurnojv@gmail.com>
2021-11-27 16:42:19 +0200

    Connect MongoDB To Our App

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//connect db
+connectDB();
 
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the contact keeper API" })
 );
 
 //define routes
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
