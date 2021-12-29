b63fa107844e9f46071bc00ed142292042db6900
unknown <chepurnojv@gmail.com>
2021-12-29 18:29:17 +0200

    Add Contact

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -8,7 +8,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
-      render={(propr) =>
+      render={(props) =>
         !isAuthenticated && !loading ? (
           <Redirect to="/login" />
         ) : (
